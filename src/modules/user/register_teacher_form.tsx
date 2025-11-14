72953af5f8995835a81d344d3a03d4dfd0cc2038
haiconkhi <165147723+haiconkhi@users.noreply.github.com>
2025-11-14 21:39:35 +0700

    fix bug

---
+++ src/modules/user/register_teacher_form.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import UserLayout from "./layout/layout";
+import RegisterFormTeacher from "./registerform_teacher";
+
+export default function RegisterTeacherFormPage() {
+	return (
+		<UserLayout>
+			{/* page title */}
+			<div className="mx-auto max-w-3xl px-4">
+				<h1 className="mb-6 mt-2 text-3xl font-semibold">Đăng ký làm giảng viên</h1>
+			</div>
+
+			{/* existing form component */}
+			<RegisterFormTeacher />
+
+			{/* spacing */}
+			<div className="mt-12" />
+		</UserLayout>
+	);
+}
